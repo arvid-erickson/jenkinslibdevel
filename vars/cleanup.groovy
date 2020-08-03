cedd9251d885210f5b7246615ec2b8e13a6faebb
Arvid Erickson <arvid.erickson@gmail.com>
2020-08-03 14:27:39 -0700

    Create cleanup.groovy

---
+++ vars/cleanup.groovy
@@ -0,0 +1,16 @@
+if (currentBuild.rawBuild.getCauses().contains("UserId") || (currentBuild.rawBuild.getCauses().contains("RestoreFromCheckpointCause")){
+  node('cdPipelineLINUX') {
+    if (env.workspace == '' ){
+      env.workspace = pws()
+    }
+    
+    if ("RELEASE".equals(env.JOB_TYPE)){
+      println(""
+      sh ""
+    } else if ("DELIVERY".equals(env.JOB_TYPE)){
+      println(""
+      sh ""
+    }
+  }
+}
+}
