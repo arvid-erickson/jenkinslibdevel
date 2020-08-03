efd84ae9b353c0031e973e2f7f0ac314ad784957
Arvid Erickson <arvid.erickson@gmail.com>
2020-08-03 16:13:14 -0700

    test with local node set

--- vars/cleanup.groovy
+++ vars/cleanup.groovy
@@ -3,9 +3,10 @@ def call()
   if (currentBuild.rawBuild.getCauses().contains("UserId") || (currentBuild.rawBuild.getCauses().contains("RestoreFromCheckpointCause")){
   
   //  node('cdPipelineLINUX') {
+    node('$ENV.JENKINS_URL') {
       if (env.workspace == '' ){
         env.workspace = pwd()
-      } */
+      } 
       println "value of workspace is ${env.workspace}"
    
       if ("RELEASE".equals(env.JOB_TYPE)){
