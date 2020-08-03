fbe606753636e445bd40ed243581a31fec9780b6
Arvid Erickson <arvid.erickson@gmail.com>
2020-08-03 15:44:55 -0700

    removing node ref

--- vars/cleanup.groovy
+++ vars/cleanup.groovy
@@ -3,11 +3,11 @@ def call()
   env.workspace = ''
   /*
   if (currentBuild.rawBuild.getCauses().contains("UserId") || (currentBuild.rawBuild.getCauses().contains("RestoreFromCheckpointCause")){
-  */  
+  
     node('cdPipelineLINUX') {
       if (env.workspace == '' ){
         env.workspace = pwd()
-      }
+      } */
       println "calue of workspace is ${env.workspace}"
     
       if ("RELEASE".equals(env.JOB_TYPE)){
@@ -18,6 +18,7 @@ def call()
         sh ""
       }
     }
+                /*
   }
-                
+                */
 }
