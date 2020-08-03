d9edcb02dbc86e961a2d487e1c4e7281983e2bb7
Arvid Erickson <arvid.erickson@gmail.com>
2020-08-03 13:54:43 -0700

    testing env.workspace() call

--- vars/printparms.groovy
+++ vars/printparms.groovy
@@ -2,5 +2,7 @@
 def printParams() {
 //  env.getEnvironment().each { name, value -> println "Name: $name -> Value $value" }
 //    echo sh(script: 'env|sort', returnStdout: true)
-  sh 'printenv'
+  if env.workspace = pwd() {
+     env.workspace() 
+  }
 }
